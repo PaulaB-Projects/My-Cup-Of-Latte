18e1954fea18e77b41ce2be32cbbb13ca30ac7e8
Paula Burghelea <paulaburghelea.github@gmail.com>
2022-07-21 16:23:22 +0200

    built JS logic for the education div alignment, started working on the menu

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 const cvButton = document.getElementById("cvButton");
+const languages = document.querySelector(".languages");
+const education = document.querySelector(".education");
+const work = document.querySelector(".work-experience");
+
 cvButton.addEventListener("click", () => {
 	// SOLUTION 1:
 	// const pageHeader = document.querySelector("header");
@@ -18,3 +22,25 @@ cvButton.addEventListener("click", () => {
 	
 	location = "#main"; // ./#main
 });
+
+function educationPosition() {
+	const viewportWidth = window.innerWidth;
+	console.log(viewportWidth);
+
+	if (viewportWidth < 960)
+		education.style.marginTop = 0;
+	else {
+		const { bottom } = languages.getBoundingClientRect();
+		const { bottom : bottomWork } = work.getBoundingClientRect();
+
+		const a = bottom - bottomWork;
+		education.style.marginTop = `${a}px`;
+	}
+}
+
+window.addEventListener('resize', () => {
+	educationPosition();
+}, true);
+
+// what about when we haven't resized the window?
+educationPosition();
