a058af00f909f78b86a942bd93bf3a2eb2f98cdb
Paula Burghelea <paulaburghelea.github@gmail.com>
2022-08-06 16:12:50 +0100

    removed the use of adjustPosition and used a forEach for the array in the calibrate function

--- js/index.js
+++ js/index.js
@@ -27,6 +27,7 @@ cvButton.addEventListener("click", () => {
 	location = "#main"; // ./#main
 });
 
+/* --- Getting the position adjusted for languages and education
 function languagesPosition() {
 	const viewportWidth = window.innerWidth;
 
@@ -59,18 +60,59 @@ function educationPosition() {
 		education.style.marginTop = `${a}px`;
 	}
 }
+*/
+/***Adjusting the position (marginTop) of an HTML element***/
+function adjustPosition(element, above, beside) {
+	const viewportWidth = window.innerWidth;
+	if (viewportWidth < 960)
+		element.style.marginTop = 0;
+	else {
+		const { bottom } = above.getBoundingClientRect();
+		const { bottom : bottomProfile } = beside.getBoundingClientRect();
 
+		const b = bottom - bottomProfile;
+		if (b <= 0)
+			element.style.marginTop = `${b}px`;
+		else {
+			element.style.marginTop = 0;
+		}
+	}
+}
 // what about when we haven't resized the window?
 
 const calibrate = () => {
-	languagesPosition();
-	educationPosition();
+	[
+		[
+			languages,
+			skills,
+			profile
+		],
+		[
+			education,
+			languages,
+			work
+		]
+	].forEach(([element, above, beside]) => {		
+			const viewportWidth = window.innerWidth;
+			if (viewportWidth < 960)
+				element.style.marginTop = 0;
+			else {
+				const { bottom } = above.getBoundingClientRect();
+				const { bottom : bottomProfile } = beside.getBoundingClientRect();
+		
+				const b = bottom - bottomProfile;
+				if (b <= 0)
+					element.style.marginTop = `${b}px`;
+				else {
+					element.style.marginTop = 0;
+				}
+			}
+		}
+	)
 	window.requestAnimationFrame(calibrate);
 }
 
 calibrate();
-
-
 
 // when menu toggle is clicked ...
 
@@ -78,7 +120,6 @@ const checkbox = document.getElementById("menu-toggle");
 const menuList = document.querySelector(".menu-list");
 const menuLabel = document.querySelector(".menu");
 const body = document.body;
-
 
 function openMenu() {
 	[
