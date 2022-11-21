4e35fc11375a064124b24a6b348dd2bd6d353816
Paula Burghelea <paulaburghelea.github@gmail.com>
2022-11-21 16:41:11 +0000

    added some basic 404 error message

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ import { readFileSync as read } from "fs";
 const app = express();
 
 const PUBLIC_FOLDER = './public';
+const VIEW_FOLDER = './view';
 const DATA_FOLDER = './data';
 const PORT = 8080;
 const ENCODING = 'utf8';
+const PAGES = [
+	"home",
+	"about",
+	"contact",
+	"portofolio"
+];
 
 function open(file) {
 	return read(file, ENCODING);
@@ -28,21 +35,31 @@ function render_template(template_file, model) {
 	return renderer(...args);
 };
 
-app.get(
-	'/',
-	(_, res) => {
-		res.send(
-			render_template(
-				`${ PUBLIC_FOLDER }/index.html`,
-				load_data("main")
+for (const page of PAGES) {
+	app.get(
+		page === "home" ? "/" : `/${ page }`,
+		(_, res) => {
+			res.send(
+				render_template(
+					`${ VIEW_FOLDER }/${ page }.html`,
+					{}
+				)
 			)
-		)
-	}
-);
+		}
+	);
+}
+//research for express.js how to create a page not found
+//arrays can have arrays inside of it 
 
 app.use(
 	express.static(PUBLIC_FOLDER)
 );
+
+app.use((req, res) => {
+	res.status(404).send(
+		"<h1>Page not found on the server</h1>"
+	)
+})
 
 app.listen(
 	PORT,
